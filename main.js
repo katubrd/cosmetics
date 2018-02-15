dba7c21e4e0eddaef4f1130c29387fa1d845be2f
Katu_brd <katuthings@gmail.com>
2018-02-15 17:10:47 +0100

    Add smoothscroll, click event on menu links, update menu links on scroll

--- main.js
+++ main.js
@@ -1,37 +1,61 @@
-var navBox = document.getElementById('nav'),
-    navElems = navBox.getElementsByClassName('nav-el');
-
-    for (var i = 0; i < navElems.length; i++) {
-      navElems[i].addEventListener('click', function(){
-        var current = document.getElementsByClassName('active');
-        current[0].className = current[0].className.replace('active', "");
-        this.className += " active";
-      });
-    }
-
-var yOffset = window.pageYOffset;
+// adding active class to clicked menu element
+function addMenuActiveclass(){
+  let navBox = document.getElementById('nav'),
+      navHrefs = navBox.querySelectorAll('li a');
+      for (let i = 0; i < navHrefs.length; i++) {
+        navHrefs[i].addEventListener('click', function(){
+          let current = document.getElementsByClassName('active');
+          current[0].className = current[0].className.replace('active', "");
+          this.className += " active";
+        });
+      }
+}
+// checking height of document elements
+let yOffset = window.pageYOffset;
 console.log(yOffset);
-
-var scroll = new SmoothScroll();
-
-var smoothScrollWithoutHash = function (selector, settings) {
-	/**
-	 * If smooth scroll element clicked, animate scroll
-	 */
-	var clickHandler = function (event) {
-		var toggle = event.target.closest( selector );
+// adding a smoth scroll
+const scroll = new SmoothScroll();
+let smoothScrollWithoutHash = function (selector, settings) {
+	let clickHandler = function (event) {
+		let toggle = event.target.closest( selector );
 		console.log(toggle);
 		if ( !toggle || toggle.tagName.toLowerCase() !== 'a' ) return;
 		console.log(toggle.hash);
-		var anchor = document.querySelector( toggle.hash );
+		let anchor = document.querySelector( toggle.hash );
 		if ( !anchor ) return;
 
 		event.preventDefault(); // Prevent default click event
 		scroll.animateScroll( anchor, toggle, settings || {} ); // Animate scroll
 	};
-
 	window.addEventListener('click', clickHandler, false );
 };
+//update menu link (add active class) on scroll
+function updateMenuOnScroll(){
+    let section = document.querySelectorAll(".page"),
+        sections = {},
+        i = 0;
 
-// Run our function
+    Array.prototype.forEach.call(section, function(e) {
+      sections[e.id] = e.offsetTop;
+    });
+
+     function update() {
+      let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+      for (i in sections) {
+        if (sections[i] <= scrollPosition) {
+          document.querySelector('.active').setAttribute('class', ' ');
+          document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+        }
+      }
+    };
+    window.addEventListener('scroll', update, false);
+}
+//animating logo in header
+// function drawLogo(){
+//
+// }
+
+addMenuActiveclass();
 smoothScrollWithoutHash( 'a[href*="#"]' );
+updateMenuOnScroll();
